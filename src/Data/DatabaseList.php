68c0de8690eaa93b830fb8f19115f98203aad604
Cram42 <cram42@users.noreply.github.com>
2024-01-28 16:24:02 +0800

    Add DatabaseList

---
+++ src/Data/DatabaseList.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace WPPluginFramework\Data;
+
+use WPPluginFramework\Data\Attributes\{
+    Column,
+    Required,
+    Unique,
+};
+
+class DatabaseList extends DatabaseTable
+{
+    #[Column, Required]
+    public string $value;
+
+    #[Column, Required, Unique]
+    public string $label;
+
+    /**
+     * Trim class suffixes from name.
+     * @return string
+     */
+    #[Override]
+    protected function trimClassSuffixes(string $name): string
+    {
+        $name = parent::trimClassSuffixes($name);
+        return preg_replace('/List$/', '', $name);
+    }
+}
