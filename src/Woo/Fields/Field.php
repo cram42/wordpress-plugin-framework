c779d35be8f59c480ca9ffdd66547396e9d2f0d6
Cram42 <cram42@users.noreply.github.com>
2024-01-28 17:34:53 +0800

    Add WooCommerce metadata field

---
+++ src/Woo/Fields/Field.php
@@ -0,0 +1,193 @@
+<?php
+
+namespace WPPluginFramework\Woo\Fields;
+
+use WPPluginFramework\{
+    Logger,
+    WPFObject,
+};
+use WPPluginFramework\Events\{
+    ILoadEvent,
+    IDisableEvent,
+    IUninstallEvent,
+};
+use WPPluginFramework\Traits\RequiresWooCommerce;
+
+use function WPPluginFramework\strsuffix;
+
+abstract class Field extends WPFObject implements
+    ILoadEvent,
+    IDisableEvent,
+    IUninstallEvent
+{
+    use RequiresWooCommerce;
+
+    #region Protected Properties
+
+    protected ?string $id = null;
+    protected ?string $label = null;
+    protected ?string $description = null;
+    protected bool $description_as_tip = true;
+
+    protected string $target = FieldTarget::DEFAULT;
+
+    protected string $class = 'short';
+    protected ?string $wrapper_class = null;
+    protected ?string $style = null;
+
+    protected bool $wipe_on_disable = false;
+    protected bool $wipe_on_uninstall = true;
+
+    #endregion
+    #region Public Methods
+
+    /**
+     * Get or generate the field description.
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        if (!$this->description) {
+            $this->description = sprintf(
+                'Added by %s',
+                get_called_class()
+            );
+        }
+        return $this->description;
+    }
+
+    /**
+     * Get or generate the field ID.
+     * @return string
+     */
+    public function getID(): string
+    {
+        if (!$this->id) {
+            $temp = get_called_class();                     // My\Namespace\ProductInfoField
+            $temp = preg_replace('/Field$/', '', $temp);    // My\Namespace\ProductInfo
+            $temp = str_replace('\\', '_', $temp);          // My_Namespace_ProductInfo
+            $temp = strtolower($temp);                      // my_namespace_productinfo
+            $this->id = $temp;
+        }
+        return $this->id;
+    }
+
+    /**
+     * Get or generate the field label.
+     * @return string
+     */
+    public function getLabel(): string
+    {
+        if (!$this->label) {
+            $temp = get_called_class();                                     // My\Namespace\ProductInfoField
+            $temp = strsuffix($temp, '\\');                                 // ProductInfoField
+            $temp = preg_replace('/Field$/', '', $temp);                    // ProductInfo
+            $temp = preg_replace('/([a-z0-9])([A-Z])/', '$1 $2', $temp);    // Product Info
+            $this->label = $temp;
+        }
+        return $this->label;
+    }
+
+    /**
+     * Run when metadata is processed. Save the data here.
+     * @param $post_id The post's id. Provided by WC.
+     * @return void
+     */
+    public function onProcessMeta($post_id): void
+    {
+        Logger::debug('onProcessMeta()', get_class(), get_called_class());
+        $value = isset($_POST[$this->getID()]) ? $_POST[$this->getID()] : null;
+        $value = $this->sanitizeValue($value);
+        update_post_meta($post_id, $this->getID(), $value);
+    }
+
+    /**
+     * Run when the $target event is triggered.
+     * Invoke the function to draw the field.
+     * @return void
+     */
+    public function onShowMetaBox(): void
+    {
+        Logger::debug('onShowMetaBox()', get_class(), get_called_class());
+        $this->drawMetaBox();
+    }
+
+    #endregion
+    #region Protected Methods
+
+    /**
+     * Sanitize value for storage.
+     * @return mixed
+     */
+    protected function sanitizeValue(mixed $value): mixed
+    {
+        return $value;
+    }
+
+    /**
+     * Draw the meta box field.
+     * @return void
+     */
+    protected function drawMetaBox(): void
+    {
+        echo('<p class="form-field ' . esc_attr($this->getId()) . '_field">');
+        echo('<label for="' . $this->getId() . '">' . $this->getLabel() . '</label>');
+
+        if ($this->description_as_tip) {
+            echo(wc_help_tip($this->getDescription()));
+        }
+
+        echo('<input type="text" disabled="disabled" class="short" placeholder="' . $this->getId() . '"></input>');
+
+        if (!$this->description_as_tip) {
+            echo('<span class="description">' . $this->getDescription() . '</span>');
+        }
+
+        echo('</p>');
+    }
+
+    #endregion
+    #region Private Methods
+
+    /**
+     * Wipe any metadata created by this field.
+     * @return void
+     */
+    private function wipeData(): void
+    {
+        Logger::info(
+            sprintf('Wiping metadata id "%s".', $this->getID()),
+            get_class(),
+            get_called_class()
+        );
+        delete_metadata('post', 0, $this->getID(), true);
+    }
+
+    #endregion
+    #region ILoadEvent Implementation
+    public function onLoadEvent(): void
+    {
+        // Hook display of field
+        add_action($this->target, [$this, 'onShowMetaBox']);
+
+        // Hook saving of field
+        add_action('woocommerce_process_product_meta', [$this, 'onProcessMeta']);
+    }
+    #endregion
+    #region IDisableEvent Implementation
+    public function onDisableEvent(): void
+    {
+        if ($this->wipe_on_disable) {
+            $this->wipeData();
+        }
+    }
+    #endregion
+    #region IUninstallEvent Implementation
+    public function onUninstallEvent(): void
+    {
+        if ($this->wipe_on_uninstall) {
+            $this->wipeData();
+        }
+    }
+    #endregion
+}
---
+++ src/Woo/Fields/FieldTarget.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace WPPluginFramework\Woo\Fields;
+
+class FieldTarget
+{
+    public const DEFAULT = FieldTarget::GENERAL;
+    
+    // General
+    public const GENERAL = 'woocommerce_product_options_general_product_data';
+    public const GENERAL_DOWNLOADS = 'woocommerce_product_options_downloads';
+    public const GENERAL_EXTERNAL = 'woocommerce_product_options_external';
+    public const GENERAL_PRICING = 'woocommerce_product_options_pricing';
+    public const GENERAL_TAX = 'woocommerce_product_options_tax';
+
+    // Inventory
+    public const INVENTORY = 'woocommerce_product_options_inventory_product_data';
+    public const INVENTORY_SKU = 'woocommerce_product_options_sku';
+    public const INVENTORY_STOCK = 'woocommerce_product_options_stock';
+    public const INVENTORY_STOCK_FIELDS = 'woocommerce_product_options_stock_fields';
+    public const INVENTORY_STOCK_STATUS = 'woocommerce_product_options_stock_status';
+    public const INVENTORY_SOLD_INDIVIDUALLY = 'woocommerce_product_options_sold_individually';
+
+    // Shipping
+    public const SHIPPING = 'woocommerce_product_options_shipping_product_data';
+    public const SHIPPING_DIMENSIONS = 'woocommerce_product_options_dimensions';
+    public const SHIPPING_OPTIONS = 'woocommerce_product_options_shipping';
+
+    // Advanced
+    public const ADVANCED = 'woocommerce_product_options_advanced';
+    public const ADVANCED_REVIEWS = 'woocommerce_product_options_reviews';
+}
